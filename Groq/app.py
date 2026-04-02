9399b518e7fe8bacd4bade5ad4209311d279899d
Adarsh Pandey <pandeyadarsh2503@gmail.com>
2026-04-03 00:44:08 +0530

    #

---
+++ Groq/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.environ['GROQ_API_KEY']
+
+if "vector" not in st.session_state:
+    st.session_state.embeddings = OllamaEmbeddings()
+    st.session_state.loader = WebBaseLoader("https://docs.smith.langchain.com/")
+    st.session_state.docs = st.session_state.loader.load()
+    
